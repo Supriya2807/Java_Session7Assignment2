4b77a96095d63bc925bff8e1920f308e6549d0c6
Supriya2807 <lahadesupriya@gmail.com>
2016-06-25 16:59:07 +0530

    session7assign2
    session7assign2

---
+++ session7assign2.java
@@ -0,0 +1,24 @@
+package acadgild.assignments;
+
+public class session7assign2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+
+			String s= "ABC";
+			int i = Integer.parseInt(s);	
+			
+					
+		}
+		
+		catch(NumberFormatException e){
+			System.out.println("Cannot convert String into number :"+e);
+			
+		
+
+			
+		}
+	}
+
+}
